33870c4adfe0085d8cc3fb252364fe160a2df053
maykalgenchev <92879155+maykalgenchev@users.noreply.github.com>
2021-10-21 19:13:30 +0300

    Add mongoose library
    Add cube model

---
+++ src/config/database.js
@@ -0,0 +1,8 @@
+const mongoose = require('mongoose');
+
+
+function initDatabase(connectionString) {
+    return mongoose.connect(connectionString);
+}
+
+module.exports = initDatabase;
--- src/models/Cube.js
+++ src/models/Cube.js
@@ -1,23 +1,35 @@
-const uniqid = require('uniqid');
+const mongoose = require('mongoose');
 
-class Cube {
-    static #cubes = [];
-
-    constructor(name, description, imageUrl, difficulty) {
-        this.id = uniqid();
-        this.name = name;
-        this.description = description;
-        this.imageUrl = imageUrl;
-        this.difficulty = difficulty;
+const cubeSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    description: {
+        type: String,
+        required: true,
+        maxLength: 100,
+    },
+    imageUrl: {
+        type: String,
+        required: true,
+        // validate: /^https?:\/\//i
+        validate: {
+            validator: function (value) {
+                return /^https?:\/\//i.test(value)
+            },
+            message: 'ImageUrl is invalid!'
+        }
+    },
+    difficultyLevel: {
+        type: Number,
+        required: true,
+        min: 1,
+        max: 5,
     }
 
-    static get cubes() {
-        return Cube.#cubes.slice();
-    }
+});
 
-    static add(cube) {
-        Cube.#cubes.push(cube)
-    }
-}
+const Cube = mongoose.model('Cube', cubeSchema);
 
 module.exports = Cube;
