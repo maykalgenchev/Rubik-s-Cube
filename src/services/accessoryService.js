009d6bf8afc673fea07ac62ddfcf893b3f8d06ff
maykalgenchev <92879155+maykalgenchev@users.noreply.github.com>
2021-10-23 16:54:06 +0300

    Show accessories select on accessory attach page

--- src/services/accessoryService.js
+++ src/services/accessoryService.js
@@ -4,8 +4,12 @@ async function create(name, description, imageUrl) {
     return Accessory.create({ name, description, imageUrl });
 }
 
+async function getAll() {
+    return Accessory.find({}).lean();
+}
 const accessoryService = {
     create,
+    getAll,
 }
 
 module.exports = accessoryService;
